a773540d0ea66fa0c00976fc148830ccbbcad7ad
DeniseHuacani <139657748+DeniseHuacani@users.noreply.github.com>
2024-01-22 18:20:16 -0500

    Update main.js

--- parte final/main.js
+++ parte final/main.js
@@ -5,13 +5,12 @@ window.addEventListener('load', showWelcome);
 function showWelcome(){
   let html = '<h2>Bienvenido ' + userFullName + '</h2>\n';
   html += `
-          <p>Este sistema fue desarrollado por alumnos del primer año de la Escuela Profesional de Ingeniería de Sistemas, de la Universidad Nacional de San Agustín de Arequipa</p>
           <p>Integrantes:</p>
           <ul>
             <li>Huacani Jara Denise Andrea</li>
-            <li></li>
-            <li></li>
-            <li></li>
+            <li>Carbajal Gonzales Diego Alejandro </li>
+            <li>Vilca Flores Johan</li>
+            <li>Cahua Soto Franco Jesus</li>
             <li></li>
           </ul>`;
   document.getElementById('main').innerHTML = html;
@@ -20,8 +19,9 @@ function showWelcome(){
 function showMenuUserLogged(){
   console.log('showMenuUserLogged está siendo llamada');
   let html = "<p onclick='showWelcome()'>Inicio</p>\n"+
-    "<p onclick='doList()'>Lista de Páginas</p>\n"+
-    "<p onclick='showNew()'>Página Nueva</p>\n"
+    "<p onclick='lista()'>Lista de Páginas</p>\n"+
+    "<p onclick='formNuevoArticulo()'>Página Nueva</p>\n"
   document.getElementById('menu').innerHTML = html;
 }
+
 
