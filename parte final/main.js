d5bb8186ea306ac444dfd68dafd5f8b136889a1f
DeniseHuacani <139657748+DeniseHuacani@users.noreply.github.com>
2024-01-28 14:47:06 -0500

    Update main.js

--- parte final/main.js
+++ parte final/main.js
@@ -1,27 +1,19 @@
 var userFullName = '';
 var userKey = '';
 
-window.addEventListener('load', showWelcome);
+//window.addEventListener('load',showWelcome);
 function showWelcome(){
   let html = '<h2>Bienvenido ' + userFullName + '</h2>\n';
-  html += `
-          <p>Integrantes:</p>
-          <ul>
-            <li>Huacani Jara Denise Andrea</li>
-            <li>Carbajal Gonzales Diego Alejandro </li>
-            <li>Vilca Flores Johan</li>
-            <li>Cahua Soto Franco Jesus</li>
-            <li></li>
-          </ul>`;
   document.getElementById('main').innerHTML = html;
 }
 
-function showMenuUserLogged(){
+function showMenuUserLogged() {
   console.log('showMenuUserLogged está siendo llamada');
-  let html = "<p onclick='showWelcome()'>Inicio</p>\n"+
-    "<p onclick='lista()'>Lista de Páginas</p>\n"+
-    "<p onclick='formNuevoArticulo()'>Página Nueva</p>\n"
-  document.getElementById('menu').innerHTML = html;
+  let html = "<div id='menu'>"+
+    "<p class='menu' onclick='lista()'>Lista de Páginas</p>\n"+
+    "<p class='menu' onclick='formNuevoArticulo()'>Página Nueva</p>\n"+
+    "</div>";
+  let inicioElem = document.getElementById('inicio');
+  inicioElem.innerHTML = html;
+  inicioElem.style.width = '200px'; // Cambia el ancho
 }
-
-
