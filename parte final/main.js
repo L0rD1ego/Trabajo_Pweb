5be58298c5709717cbe1d7c34c196a31494827a4
DeniseHuacani <139657748+DeniseHuacani@users.noreply.github.com.>
2024-02-02 20:19:59 -0500

    Cambios en css

--- parte final/main.js
+++ parte final/main.js
@@ -1,7 +1,6 @@
 var userFullName = '';
 var userKey = '';
 
-//window.addEventListener('load',showWelcome);
 function showWelcome(){
   let html = '<h2>Bienvenido ' + userFullName + '</h2>\n';
   document.getElementById('main').innerHTML = html;
@@ -10,10 +9,24 @@ function showWelcome(){
 function showMenuUserLogged() {
   console.log('showMenuUserLogged está siendo llamada');
   let html = "<div id='menu'>"+
-    "<p class='menu' onclick='lista()'>Lista de Páginas</p>\n"+
-    "<p class='menu' onclick='formNuevoArticulo()'>Página Nueva</p>\n"+
+      "<div class='usuario'>"+
+        "<div class ='icon-usuario'>"+
+            "<i class='bx bxs-user'></i>"+
+        "</div>"+
+        "<p class ='nombre-usuario'>"+ userFullName + "</p>"+
+      "</div>"+
+      "<p class='menu' onclick='lista()'>Lista de Páginas</p>\n"+
+      "<p class='menu' onclick='formNuevoArticulo()'>Página Nueva</p>\n"+
     "</div>";
   let inicioElem = document.getElementById('inicio');
   inicioElem.innerHTML = html;
-  inicioElem.style.width = '200px'; // Cambia el ancho
+  let formContainerElem = document.querySelector('.form-container');
+  formContainerElem.style.width = '1500px'; //  ancho
+  formContainerElem.style.height = '680px';
+  formContainerElem.style.marginTop = '10px';
+  let mainElem2 =document.querySelector('.main');
+  mainElem2.style.width='80%';
+
 }
+
+
--- parte final/myScript.js
+++ parte final/myScript.js
@@ -1,56 +1,27 @@
 /**
- * Esta función muestra un formulario de login
- * LLamando a la  función doLogin
- * Modifica el tag div con id main en el html
- */
-function showLogin() {
-
-    let html = '<h2>Bienvenido ' + userFullName + '</h2>\n';
-    html += `
-    <form class="formulario">
-        <label for="user">Usuario:</label>
-        <input type="text" id="user" name="user" required>
-        <br><br>
-        <label for="password">Contraseña:</label>
-        <input type="password" id="password" name="password" required>
-        <br><br>
-        <input type="button" onclick="doLogin()" value="Iniciar Sesión" class='boton-log'>
-    </form>`;
-    //document.getElementById('main')..appendChild(centerContainer);
-    const fondo = document.createElement('div');
-    fondo.classList.add('fondo-oscuro');
-
-    const contcentro = document.createElement('div');
-    contcentro.classList.add('contenedor-centro');
-    contcentro.innerHTML = html;
-
-    document.body.appendChild(fondo);
-    fondo.appendChild(contcentro);
-}
-/**
  * Esta función recolecta los valores ingresados login.pl
  * La respuesta del CGI es procesada por la función loginResponse
  */
 function doLogin(){
-    console.log('dologin esta ejecutando');
+  console.log('dologin esta ejecutando');
 
-    let user = document.getElementById('user').value;
-    let password = document.getElementById('password').value;
-    // Realizar una solicitud Fetch al servidor
-    fetch('login.pl', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        body: 'user=' + encodeURIComponent(user) + '&password=' + encodeURIComponent(password),
-    })
-    .then(response => response.text())
-    .then(data => {
-         var xml = (new window.DOMParser()).parseFromString(data, "text/xml");
-         console.log('Respuesta del servidor:', xml);
-         loginResponse(xml);
-    })
-    .catch(error => console.error('Error:', error));
+  let user = document.getElementById('user').value;
+  let password = document.getElementById('password').value;
+  // Realizar una solicitud Fetch al servidor
+  fetch('login.pl', {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: 'user=' + encodeURIComponent(user) + '&password=' + encodeURIComponent(password),
+  })
+  .then(response => response.text())
+  .then(data => {
+       var xml = (new window.DOMParser()).parseFromString(data, "text/xml");
+       console.log('Respuesta del servidor:', xml);
+       loginResponse(xml);
+  })
+  .catch(error => console.error('Error:', error));
 }
 /**
  * Se inicializa la variable userFullName y userKey (e usuario)
@@ -83,15 +54,11 @@ function loginResponse(xml) {
 
 }
 /**
- * Actualizar el tag con id userName en el HTML
  * Invoca las functiones showWelcome y showMenuUserLogged
  */
 function sesionIniciada(userFullName, userKey){
     console.log('sesionIniciada');
-    // Actualiza el tag con id userName en el HTML con el nombre del usuario
-    document.getElementById('userName').textContent = userFullName;
-    // Llama a las funciones showWelcome y showMenuUserLogged
-    showWelcome();
+    lista();
     showMenuUserLogged();
 }
 /**
@@ -101,26 +68,26 @@ function sesionIniciada(userFullName, userKey){
  * función crearCuenta
  * */
 function formCrearCuenta(){
-    let html =  `
-      <form class='info-iniciar-sesion' >
-      <h2>Crear Cuenta </h2>
-      <input type="text" placeholder ="Usuario" id="user" name="user" required>
-      <br><br>
-      <input type="password" placeholder ="Contraseña" id="password" name="password" required>
-      <br><br>
-      <input type="text" placeholder ="Apellido" id="lastName" name="lastName" required>
-      <br><br>
-      <input type="text"  placeholder ="Nombre"  id="firstName" name="firstName" required>
-      <br><br>
-      <input type="button" onclick="crearCuenta()" value ="Crear Cuenta">
-     </form>`;
-     document.getElementById('main').innerHTML = html;
+  let html =  `
+    <form class='info-iniciar-sesion' >
+    <h2>Crear Cuenta </h2>
+    <input type="text" placeholder ="Usuario" id="user" name="user" required>
+    <br><br>
+    <input type="password" placeholder ="Contraseña" id="password" name="password" required>
+    <br><br>
+    <input type="text" placeholder ="Apellido" id="lastName" name="lastName" required>
+    <br><br>
+    <input type="text"  placeholder ="Nombre"  id="firstName" name="firstName" required>
+    <br><br>
+    <input type="button" onclick="crearCuenta()" value ="Crear Cuenta">
+   </form>`;
+   document.getElementById('main').innerHTML = html;
 
 }
 /* Esta función extraerá los datos ingresados en el formulario de
- * registro de nuevos usuarios e invocará al CGI register.pl
- * la respuesta de este CGI será procesada por loginResponse.
- */
+* registro de nuevos usuarios e invocará al CGI register.pl
+* la respuesta de este CGI será procesada por loginResponse.
+*/
 function crearCuenta(){
   const user = document.getElementById('user').value;
   const password = document.getElementById('password').value;
@@ -137,8 +104,6 @@ function crearCuenta(){
   })
     .then(response => response.text());
     sesionIniciada(user,password);
-    //showWelcome();
-    //showMenuUserLogged();
 
 }
 
@@ -161,12 +126,10 @@ function lista(){
     .then(response => response.text())
     .then(data => {
         console.log('Respuesta del servidor:', data);
-        //const xml = new DOMParser().parseFromString(data, 'text/xml');
         var xml = (new window.DOMParser()).parseFromString(data, "text/xml");
         mostrarLista(xml);
     })
     .catch(error => console.error('Error:', error));
-
 
 }
 
@@ -181,14 +144,13 @@ function mostrarLista(xml){
   console.log('mostrarLista');
 
   const articles = xml.querySelectorAll('article');
-  const contenedor = document.getElementById('main');
+  const mainElem = document.getElementById('main');
   if (articles.length === 0) {
-    contenedor.innerHTML = '<p>No hay articulos.</p>';
+    mainElem.innerHTML = '<p>No hay articulos.</p>';
     return;
   }
 
-  contenedor.innerHTML = '';
-
+  mainElem.innerHTML = '';
   articles.forEach(article => {
     const owner = article.querySelector('owner').textContent;
     const title = article.querySelector('title').textContent;
@@ -203,10 +165,16 @@ function mostrarLista(xml){
       <button onclick="editarArchivo('${owner}', '${title}')"class='boton-Op'>Editar</button>
     `;
 
-    contenedor.appendChild(articulos);
+    mainElem.appendChild(articulos);
   });
+  mainElem.style.flexDirection = 'column';
+  mainElem.style.display='block';
+  mainElem.style.padding='50px';
+  mainElem.style.maxHeight = '680px'; //  altura máxima
+  mainElem.style.overflowY = 'scroll';
+  mainElem.style.textAlign = 'left';
+  mainElem.style.alignItems = 'normal';
 }
-
 /**
  * Dos botones
  * - Enviar, que invoca a nuevoArticulo
@@ -251,9 +219,8 @@ function nuevoArticulo(){
       lista();
     })
     .catch(error => console.error('Error en nuevoArticulo:', error));
-
-  // Devolver false para prevenir la recarga de la página
-  return false;
+// Devolver false para prevenir la recarga de la página
+return false;
 }
 
 /*
@@ -271,8 +238,8 @@ function verArchivo(owner, title){
     .then(response => response.text())
     .then(data => {
         console.log('verArchivo:', data);
-        document.getElementById('main').innerHTML=data;
-
+        let mainElem = document.getElementById('main');
+        mainElem.innerHTML = data;
     })
     .catch(error => {
         console.error('Error en verArchivo:', error);
@@ -283,28 +250,28 @@ function verArchivo(owner, title){
  * Esta función invoca al CGI delete.pl
  */
 function eliminarArchivo(owner, title){
-   fetch('delete.pl', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    body: `usuario=${encodeURIComponent(owner)}&titulo=${encodeURIComponent(title)}`,
-  })
-    .then(response => response.text())
-    .then(data => {
-      // Procesar la respuesta del servidor si es necesario
-      console.log('Respuesta del servidor (eliminarArchivo):', data);
-      alert('Archivo eliminado');
-      lista(); // Actualizar la lista después de eliminar
-    })
-    .catch(error => console.error('Error en eliminarArchivo:', error));
+  fetch('delete.pl', {
+   method: 'POST',
+   headers: {
+     'Content-Type': 'application/x-www-form-urlencoded',
+   },
+   body: `usuario=${encodeURIComponent(owner)}&titulo=${encodeURIComponent(title)}`,
+ })
+   .then(response => response.text())
+   .then(data => {
+     // Procesar la respuesta del servidor si es necesario
+     console.log('Respuesta del servidor (eliminarArchivo):', data);
+     alert('Archivo eliminado');
+     lista(); // Actualizar la lista después de eliminar
+   })
+   .catch(error => console.error('Error en eliminarArchivo:', error));
 
 }
 
 /*
- * Esta función recibe los datos del articulo a editar e invoca al cgi
- * article.pl la respuesta del CGI es procesada por responseEdit
- */
+* Esta función recibe los datos del articulo a editar e invoca al cgi
+* article.pl la respuesta del CGI es procesada por responseEdit
+*/
 function editarArchivo(owner, title){
   fetch('article.pl', {
     method: 'POST',
@@ -322,6 +289,7 @@ function editarArchivo(owner, title){
     .catch(error => console.error('Error en editarArchivo:', error));
 
 }
+
 /*
  * - Actualizar que invoca a actualizarArt
  * - Cancelar que invoca a lista
@@ -379,4 +347,3 @@ function actualizarArt(title){
     console.error('Las variables userFullName y userKey no están definidas.');
   }
 }
-
